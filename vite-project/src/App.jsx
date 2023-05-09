eee7a0bc63c3ae6d4016c744b776a172962ea19f
Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>
2023-05-09 15:48:04 -0400

    Adding error makers

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,33 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import * as Sentry from "@sentry/react";
 import './App.css'
+
+Sentry.init({
+  dsn: "https://68a58f8a4dfa4fe6b6f3fc7435aa302b@o4505155651895296.ingest.sentry.io/4505155655368704",
+  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+class ValidationError extends Error {
+  constructor(message) {
+    super(message); // (1)
+    this.name = "ValidationError"; // (2)
+  }
+}
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const handleClick = () => {
+    setCount((count) => count + 1)
+    throw new ValidationError('This is the error message')
+  };
 
   return (
     <>
@@ -18,7 +41,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={handleClick}>
           count is {count}
         </button>
         <p>
@@ -32,4 +55,4 @@ function App() {
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
